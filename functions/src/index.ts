b9ee2f2e5a295e06261e6167fecdee4b068905eb
mikeaxle <mikeaxle2006@gmail.com>
2018-12-04 23:26:31 +0200

    create non-admin user and send welcome email

---
+++ functions/src/index.ts
@@ -0,0 +1,68 @@
+import * as functions from 'firebase-functions'
+import * as admin from 'firebase-admin'
+import * as sendgrid from '@sendgrid/mail'
+
+// firebase admin init admin
+admin.initializeApp()
+
+// set app name
+const APP_NAME = 'Coffee Shop App';
+
+// set send grid api key
+const API_KEY = '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'
+
+// Creates new non-admin user
+export const createUser = functions.https.onRequest((request, response) => {
+    // request params
+    const user = {
+        email: request.body.email,
+        password: request.body.password,
+        phoneNumber: request.body.phoneNumber,
+        displayName: request.body.firstName,
+        photoURL: request.body.image
+    }
+
+    // create new user with automated password
+    admin.auth().createUser(user)
+    .then((newUser) => {
+        console.log("New user created")
+        // send welcome email
+        sendWelcomeEmail(newUser.email, newUser.displayName, user.password, request.body.role)
+        .then(() => {
+            console.log("Welcome email sent")
+            response.send("New user created & welcome email sent to: " + user.email)
+        })
+        .catch((err) => {
+            console.log("Welcome email sending failed: " + err)
+            response.send("New user created but welcome email failed to send to: " + user.email)
+        })
+        
+    })
+    .catch((err) => {
+        console.log(err)
+        response.send("New user creation failed: " + err)
+        
+    })
+})
+
+// Sends a welcome email to the given user.
+function sendWelcomeEmail (email, displayName, password, role) {
+    // set api key for send grid
+    sendgrid.setApiKey(API_KEY)
+
+    // create message object
+    const msg = {
+        to: email,
+        from: `${APP_NAME} <noreply@coffee-shop-app-d8f60.firebaseapp.com>`,
+        subject: `Welcome to the ${APP_NAME}!`,
+        html:   `<h2>Hey ${displayName || ''}! Welcome to ${APP_NAME}.</h2>
+                <h3>I hope you will enjoy our service.<h3/>
+                <p>You have received this email because you have been added as a staff member (${role}) <br>
+                on a store managed by ${APP_NAME} by its Administrator. Please download the ${APP_NAME} from this <a href='http://google.com'>link</a> and login to access the store.</p>
+                <p>Your login email address is: ${email}</p>
+                <p>Your randomly generated password is: ${password}</p>`
+    }
+
+    // send message
+    return sendgrid.send(msg)
+  }
